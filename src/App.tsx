407446ce6db58db6809a5a17226a1ba7762a6c6e
이나령 <thisisme20@daum.net>
2023-08-16 16:10:05 +0900

    chore: svgr 사용 관련 설정, 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import './index.css';
-import SearchIcon from './assets/search.svg';
+import { ReactComponent as SearchIc } from "./assets/search.svg";
 
 interface SearchResult {
   sickCd?: string,
@@ -101,7 +101,7 @@ function App() {
             onChange={handleInputChange}
           ></input>
           <button className="relative z-[2] rounded-r border-2 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0">
-            검색
+            <SearchIc/>
           </button>
         </div>
       </form>
