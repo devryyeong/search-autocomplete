5d06387f317faeb7b9e5784af6704343049f9b5d
이나령 <thisisme20@daum.net>
2023-08-17 18:05:02 +0900

    fix: fetch를 axios 사용하여 변경 후 api 요청 함수 분리

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { ReactComponent as SearchIc } from "../assets/search.svg";
 import SearchBox from "../components/SearchBox";
+import { useDebounce } from "../hooks/useDebounce";
 
 interface SearchResult {
   sickCd?: string;
@@ -13,18 +14,32 @@ export const SearchPage = () => {
 
   const cacheKey = `sick_${query}`;
 
-  const fetchData = async (): Promise<SearchResult> => {
-    const apiUrl = `http://localhost:4000/sick?q=${query}`;
-    const response = await fetch(apiUrl);
-    return response.json();
-  };
-
-  const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setQuery(e.currentTarget.value);
-  };
+  const handleInputChange = useDebounce((e: React.FormEvent<HTMLInputElement>) => {
+    // setQuery(e.currentTarget.value);
+    const value = e.target.value.trim();
+    if (value === '') {
+      setVisible(false);
+    } else {
+      fetchData(query)
+    }
+  }, 1000);
 
   const handleInputClick = () => {
     setVisible(!visible);
+  };
+
+  const handleSearchClick = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    try {
+      fetchData()
+      .then(() => console.log("A"))
+      .catch(err => console.log(err));
+      
+    } catch {
+      throw new Error("ERROR");
+    }
+    console.log(query);
+    setQuery('');
   };
 
   return (
@@ -39,7 +54,10 @@ export const SearchPage = () => {
             onChange={handleInputChange}
             onClick={handleInputClick}
           ></input>
-          <button className="relative z-[2] rounded-r border-2 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0">
+          <button
+            className="relative z-[2] rounded-r border-2 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
+            onClick={handleSearchClick}
+          >
             <SearchIc />
           </button>
         </div>
