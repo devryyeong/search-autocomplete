a0f80c54d291699845382484445a0ea2153a6724
이나령 <thisisme20@daum.net>
2023-08-17 12:01:31 +0900

    chore: SearchBox와 SearchInput 컴포넌트 분리

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SearchBox = () => {
+  return (
+    <ul className="p-1 rounded-md border-solid border-2 border-gray-700 text-sm text-gray-600 divide-y divide-slate-300">
+      <li>bbb</li>
+      <li>ggg</li>
+      <li>jjj</li>
+    </ul>
+  );
+}
+
+export default SearchBox
---
+++ src/hooks/useCachedData.tsx
@@ -0,0 +1,30 @@
+export default function useCachedData<T>(
+  cacheKey: string,
+  fetchData: () => Promise<T>
+): T | undefined {
+  const [cachedData, setCachedData] = useState<T>();
+
+  useEffect(() => {
+    const fetchDataAndCache = async () => {
+      const cachedData = localStorage.getItem(cacheKey);
+      if (cachedData) {
+        console.log("로컬 캐시에서 데이터를 가져옵니다.");
+        setCachedData(JSON.parse(cachedData));
+        return;
+      }
+
+      try {
+        const data = await fetchData();
+        localStorage.setItem(cacheKey, JSON.stringify(data));
+        console.log("API에서 데이터를 가져옵니다.", cachedData);
+        setCachedData(data);
+      } catch (error) {
+        console.error("오류 발생:", error);
+      }
+    };
+
+    fetchDataAndCache();
+  }, []);
+
+  return cachedData;
+}
