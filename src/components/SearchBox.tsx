190ae37c85fef916ac6a707a77466c2abff34c91
이나령 <thisisme20@daum.net>
2023-08-17 12:18:29 +0900

    chore: favicon 변경

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+interface SearchBoxProps {
+  visible: boolean;
+  closeBox: void;
+}
+
 const SearchBox = () => {
   return (
     <ul className="p-1 rounded-md border-solid border-2 border-gray-700 text-sm text-gray-600 divide-y divide-slate-300">
